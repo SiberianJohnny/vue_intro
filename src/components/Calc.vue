591f1c18f7ed1b2cd87d6baabbb65648bb2dfd88
Siberian Johnny <allidon93@gmail.com>
2021-08-03 12:17:55 +0700

    Logs and operations refactoring

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -2,13 +2,26 @@
   <div class="calc">
     <input type="number" v-model.number="number1" />
     <input type="number" v-model.number="number2" />
+    <br />
     = {{ result }}
-    <button @click="sum">+</button>
-    <button @click="subtraction">-</button>
-    <button @click="multi">*</button>
-    <button @click="division">/</button>
-    <button @click="pow">Exponentiation</button>
-    <button @click="int">Integer Division</button>
+    <br />
+
+    <div class="error" v-if="!!error">Ошибка: {{ error }}</div>
+
+    <button
+      v-for="item in operations"
+      :key="item"
+      @click="calculate(item)"
+      :title="item"
+    >
+      {{ item }}
+    </button>
+
+    <div class="logs">
+      Logs:
+      <br />
+      <div v-for="(log, id) in logs" :key="id">{{ log }}</div>
+    </div>
   </div>
 </template>
 
@@ -19,8 +32,37 @@ export default {
     number1: 0,
     number2: 0,
     result: 0,
+    error: "",
+    operations: ["+", "-", "*", "/", "Exponentiation", "Integer Division"],
+    logs: {},
   }),
   methods: {
+    calculate(operation) {
+      this.error = "";
+      switch (operation) {
+        case "+":
+          this.sum();
+          break;
+        case "-":
+          this.subtraction();
+          break;
+        case "*":
+          this.multi();
+          break;
+        case "/":
+          this.division();
+          break;
+        case "Exponentiation":
+          this.pow();
+          break;
+        case "Integer Division":
+          this.int();
+          break;
+      }
+      const key = Date.now();
+      const value = `${this.number1} ${operation} ${this.number2} = ${this.result}`;
+      this.$set(this.logs, key, value);
+    },
     sum() {
       this.result = this.number1 + this.number2;
     },
@@ -31,7 +73,11 @@ export default {
       this.result = this.number1 * this.number2;
     },
     division() {
-      this.result = this.number1 / this.number2;
+      if (this.number1 === 0 || this.number2 === 0) {
+        this.error = "Я вам запрещаю делить на 0.";
+      } else {
+        this.result = this.number1 / this.number2;
+      }
     },
     pow() {
       this.result = Math.pow(this.number1, this.number2);
